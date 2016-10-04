8d9d97a73e62ea0b93bc891ece9c93cc64812713
robmcinnis <rob.mcinnis@iamota.com>
2016-10-04 14:46:58 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-# A sample Bash script, by Rob
-echo Hello World!
-node -v
-ls
-npm install selenium-webdriver
-node GoogleSearch.js
+    #!/bin/bash
+    # A sample Bash script, by Rob
+    echo Hello World!
+    npm install selenium-webdriver
+    npm install chromedriver
+    node -v
+    ls
