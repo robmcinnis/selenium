754760c3fa856fc6ecbdbba57e01dc99cb16691c
robmcinnis <rob.mcinnis@iamota.com>
2016-10-04 10:54:59 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,3 +2,7 @@
     #!/bin/bash
     # A sample Bash script, by Rob
     echo Hello World!
+    npm install selenium-webdriver
+    npm install chromedriver
+    nvm install 5.6.0
+    node -version
