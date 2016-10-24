f72b033361e7ffeddeeb0a65a6be1f4a8e33fc41
robmcinnis <rob.mcinnis@iamota.com>
2016-10-24 10:58:11 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,6 +6,9 @@
       cd ..
       ls
       
-      npm install selenium-webdriver
-      npm install wd-sync
+      #npm install selenium-webdriver
+      nvm install 5.6
+      gem install selenium-webdriver
+      
+      #npm install wd-sync
       #node GoogleSearch.js
