5cdd4841b9228752ff525bfec6c62f57926c664f
robmcinnis <rob.mcinnis@iamota.com>
2016-11-02 16:43:43 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,23 +1,25 @@
      #!/bin/bash
       # A sample Bash script, by Rob
-      echo Starting Automated Tests!
-      
-      ls
-      cd ~/src/github.com/iamota/iamota-components/automated-tests/
-      ls
+      echo This is where our automated tests would go, maybe.
+ 
+      #ls
+      #cd ~/src/github.com/iamota/iamota-components/automated-tests/
+      #ls
       #Update versions of PhantomJs and CasperJs
-      npm install phantomjs@2.1 -g
-      npm install -g casperjs
+      #npm install phantomjs@2.1 -g
+      #npm install -g casperjs
       
-      #########cant run these test here, they were failing.##########
-      casperjs test kaltire-homepage.js
-      casperjs test kaltire-global-search.js
-      casperjs test kaltire-tire-selector.js
-      casperjs test kaltire-homepage-location.js
+      #########I i try to run these tsts in sucession, only the first test actually runs##########
+      #casperjs test kaltire-homepage.js
+      #casperjs test kaltire-global-search.js
+      #casperjs test kaltire-tire-selector.js
+      #casperjs test kaltire-homepage-location.js
       
+      #Install packages to run Selenium Webdriver
       #npm install selenium-webdriver
       #nvm install 5.6
       #gem install selenium-webdriver
-      
       #npm install wd-sync
+      
+      #We would put any Selenium Webdriver tests here
       #node GoogleSearch.js
