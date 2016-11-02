2b6de80242e66adac325eb6a3071f6f2cb60b641
robmcinnis <rob.mcinnis@iamota.com>
2016-11-02 10:10:19 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,14 +1,11 @@
      #!/bin/bash
       # A sample Bash script, by Rob
-      echo Hello World!
-      node -v
-      ls
-      cd ..
-      ls
+      echo Starting Automated Tests!
+      casperjs test kaltire-homepage-location.js
       
       #npm install selenium-webdriver
-      nvm install 5.6
-      gem install selenium-webdriver
+      #nvm install 5.6
+      #gem install selenium-webdriver
       
       #npm install wd-sync
       #node GoogleSearch.js
